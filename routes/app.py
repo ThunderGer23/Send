441f161b4ae4c774501c13e78891611ed69fa74e
ThunderGer <jaames11@hotmail.com>
2023-04-21 11:56:24 -0600

    Listo el send

--- routes/app.py
+++ routes/app.py
@@ -13,28 +13,16 @@ def getTest():
     ids = []
     for i in range(len(lista)):
         with open(f'./document/{lista[i]}', 'rb') as f:
-            test = conn.local.files.insert_one({
-                "name": lista[i],
-                "data": f.read()
-            }).inserted_id
-            ids.append(str(test))
-            print(test)
-            test1 = conn.local.files.find_one({"_id": test})
-            print(test1)
-
+            ids.append(str(conn.local.files.insert_one({ "name": lista[i], "data": f.read() }).inserted_id))
     value = {
         "id": ids,
     }
-    
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json"
     }
-
-    print(f'Enviando {value}')
     response = requests.post("https://recive-production.up.railway.app/probando", headers=headers, json=value)
-    # response = requests.post("http://localhost:8001/probando", headers=headers, json=value)
-    
+    # response = requests.post("http://localhost:8005/probando", headers=headers, json=value)    
     return 'ok'
 
 @routes.post('/probando')
